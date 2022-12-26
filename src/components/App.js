b0ff1376f13c62511ad5ea5ded6a26425e539698
Kristian <95530852+Krisdziam@users.noreply.github.com>
2022-12-26 20:46:57 +0200

    r

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,16 @@ import 'react-toastify/dist/ReactToastify.css';
 import Layout from './Layout/Layout';
 import Loader from './Loader/Loader';
 
-const HomePage = lazy(() => import('./HomePage/HomePage'));
+
+const LoginPage = lazy(() =>
+  import('./Pages/LoginPage/LoginPage')
+);
+const RegisterPage = lazy(() =>
+  import('./Pages/RegisterPage/RegisterPage')
+);
+const HomePage = lazy(() =>
+  import('./Pages/HomePage/HomePage')
+);
 const ContactsList = lazy(() =>
   import('./ContactsList/ContactsPage')
 );
@@ -16,7 +25,15 @@ export function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
-          <Route path='contacts' element={<ContactsList />} />
+          <Route
+            path="contacts"
+            element={<ContactsList />}
+          />
+          <Route path="login" element={<LoginPage />} />
+          <Route
+            path="register"
+            element={<RegisterPage />}
+          />
         </Route>
       </Routes>
       <ToastContainer />
