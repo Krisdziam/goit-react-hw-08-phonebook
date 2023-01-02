a8d4c2ed73402fbfb35e2470f2d42a16ab1780ff
Kristian <95530852+Krisdziam@users.noreply.github.com>
2023-01-02 21:16:20 +0200

    login ready

--- src/components/Pages/LoginPage/LoginPage.jsx
+++ src/components/Pages/LoginPage/LoginPage.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { useDispatch } from 'react-redux';
+import { logIn } from 'redux/auth/operations';
 
 export default function LoginPage() {
+  const dispatch = useDispatch();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    dispatch(
+      logIn({
+        email: form.elements.email.value,
+        password: form.elements.password.value,
+      })
+    );
+    form.reset();
+  };
   return (
-    <form  autoComplete="off">
+    <form onSubmit={handleSubmit} autoComplete="off">
     <label >
       Email
       <input type="email" name="email" />
