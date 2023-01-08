7fc72de4e8634b136af68d0b144ee845e8558c5e
Kristian <95530852+Krisdziam@users.noreply.github.com>
2023-01-08 16:38:59 +0200

    Update authNav.jsx

--- src/components/authNav/authNav.jsx
+++ src/components/authNav/authNav.jsx
@@ -1,32 +1,31 @@
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
- function AuthNav() {
+export default function AuthNav ()  {
   return (
     <div>
-      <NavLink to="/register">Register</NavLink>
-      <NavLink to="/login">Log In</NavLink>
+      <Links to="/register">Register</Links>
+      <Links to="/login">Log In</Links>
     </div>
   );
-}
+};
 
-export default AuthNav
-// export const Links = styled(NavLink)`
-//   display: inline-block;
-//   text-decoration: none;
-//   padding-left: 20px;
-//   margin: 0;
-//   font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
-//   font-weight: 400;
-//   font-size: 1rem;
-//   line-height: 1.5;
-//   letter-spacing: 0.00938em;
-//   color: #fff;
-//   transition: color 250ms cubic-bezier(0.4, 0, 0.2, 1);
-//   &:hover {
-//     color: rgb(200, 200, 200);
-//   }
-//   &.active {
-//     color: rgb(254, 217, 217);
-//   }
-// `;
+export const Links = styled(NavLink)`
+display: inline-block;
+text-decoration: none;
+padding-left: 20px;
+margin: 0;
+font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+font-weight: 400;
+font-size: 1rem;
+line-height: 1.5;
+letter-spacing: 0.00938em;
+color: #fff;
+transition: color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+&:hover {
+  color: rgb(200, 200, 200);
+}
+&.active {
+  color: rgb(254, 217, 217);
+}
+`;
