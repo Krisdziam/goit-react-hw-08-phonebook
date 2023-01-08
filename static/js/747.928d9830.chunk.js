0752a0767a454ed53ebb0ae9c1cc923cbb8edd3d
Krisdziam <95530852+Krisdziam@users.noreply.github.com>
2023-01-08 14:30:59 +0000

    Deploying to gh-pages from @ Krisdziam/goit-react-hw-08-phonebook@8640d7644e3b782bf04d4f5af1647fe429bd3932 🚀

---
+++ static/js/747.928d9830.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[747],{4747:function(t,e,n){n.r(e),n.d(e,{default:function(){return i}});var o=n(8870),s=n(890),r=n(184);function i(){return(0,r.jsx)(o.Z,{sx:{display:"flex",justifyContent:"center "},children:(0,r.jsx)(s.Z,{variant:"h5",sx:{fontWeight:"700"},children:"Phonebook... The best solution to save your contacts! Just try it!!!"})})}}}]);
+//# sourceMappingURL=747.928d9830.chunk.js.map
